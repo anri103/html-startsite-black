69413381daea866927d411740e50dec0a0e87b17
anri103 <ianrianri@gmail.com>
2023-08-04 17:21:22 +0300

    main js fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 //////////////////////////////////////////////////////////////////
+// [ Тень меню при скролле ]
+
+window.onscroll = function() {
+    const windowScroll = window.scrollY;
+	const fixedMenu = document.querySelector('.sticky-heading');
+    if(windowScroll > 200) {
+        fixedMenu.classList.add('shadow-sm');
+    }
+    else {
+        fixedMenu.classList.remove('shadow-sm');
+    }
+}
+
+//////////////////////////////////////////////////////////////////
 // [ Back to Top Button ]
 
 const scrollBtn = document.querySelector('.btn-scroll-top');
@@ -18,20 +32,6 @@ scrollBtn.addEventListener('click', () => {
         behavior: 'smooth'
     });
 });
-
-//////////////////////////////////////////////////////////////////
-// [ Тень меню при скролле ]
-
-window.onscroll = function() {
-    const windowScroll = window.scrollY;
-	const fixedMenu = document.querySelector('.sticky-heading');
-    if(windowScroll > 200) {
-        fixedMenu.classList.add('shadow-sm');
-    }
-    else {
-        fixedMenu.classList.remove('shadow-sm');
-    }
-}
 
 //////////////////////////////////////////////////////////////////
 // [ Слайдеры на странице index.html ]
@@ -128,36 +128,6 @@ const relatedProducts = new Swiper('.relatedProducts', {
 //////////////////////////////////////////////////////////////////
 // [ PHONE MASK ]
 
-document.addEventListener('DOMContentLoaded', function () {
-	
-
-    var mask1 = document.getElementById('maskPhone1')
-	if( mask1 ){
-        const maskPhone1 = IMask(
-            mask1, {
-            mask: '+{7} (000) 000-00-00'
-        });
-    }
-
-    var mask2 = document.getElementById('maskPhone2')
-	if( mask2 ){
-        const maskPhone2 = IMask(
-            mask2, {
-            mask: '+{7} (000) 000-00-00'
-        });
-    }
-
-    var mask3 = document.getElementById('maskPhone3')
-	if( mask3 ){
-        const maskPhone3 = IMask(
-            mask2, {
-            mask: '+{7} (000) 000-00-00'
-        });
-    }
-	
-});
-
-// Новый вариант
 var maskPhone = document.querySelectorAll('.maskPhone')
 var maskDate = document.querySelectorAll('.maskDate')
 
